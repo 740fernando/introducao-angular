dc247a1e02aecbe7d3fce0180f7f16c19178cb3f
Fernando <fernando.luiz92@hotmail.com>
2022-06-25 15:53:19 -0300

    Trabalhando com módulos e componentes

--- workspace/course-manager/src/app/app.component.ts
+++ workspace/course-manager/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // gera um tag que pode ser utilizada em qlq lugar
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-manager';
+  title = 'course-manager'; // atrib. do componente
+
+
+  name: String = 'Fernando'
 }
